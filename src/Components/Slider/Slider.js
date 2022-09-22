ec29789ecffc34c0a76539d30e39a0cabb4c96f2
Kévin - Noxtaria <noxtariadev@gmail.com>
2022-09-22 14:14:54 +0200

    Ajout BtnSlider

---
+++ src/Components/Slider/Slider.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+import './Slider.css'
+import dataSlider from './dataSlider'
+import BtnSlider from './BtnSlider'
+
+export default function Slider() {
+
+    const [slideAnim, setSlideAnim] = useState({
+        index: 1,
+        inProgress: false
+    })
+
+    const nextSlide = () => {
+        console.log("Next");
+    }
+
+    const prevSlide = () => {
+        console.log("Prev");
+    }
+
+  return (
+    <div className='container-slider'>
+        {dataSlider.map((obj, index) => {
+            return (
+                <div 
+                key={obj.id}
+                    className={slideAnim.index === index + 1 ?
+                    "slide active-anim" : "slide"}
+                >
+                    <img src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`} alt="" />
+                </div>
+            )
+        })}
+        <BtnSlider moveSlide={nextSlide} direction={"next"}/>
+        <BtnSlider moveSlide={prevSlide} direction={"prev"}/>
+
+    </div>
+  )
+}
